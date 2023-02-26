b80a95fde9331fcdba8bf84ca25252a262240488
CN-sensei-matt <121567143+CN-sensei-matt@users.noreply.github.com>
2023-02-26 20:33:02 +0000

    add checking

--- Cards/Card.cs
+++ Cards/Card.cs
@@ -8,6 +8,11 @@ namespace Cards
 
         public Card(int suit, int rank)
         {
+            if (suit < 0 || suit > 3)
+                throw new System.ArgumentException("Invalid suit. Must be between 0 and 3", "suit");
+            if (rank < 1 || rank > 13)
+                throw new System.ArgumentException("Invalid rank. Must be between 1 and 13", "rank");
+
             Suit = suit;
             Rank = rank;
         }
